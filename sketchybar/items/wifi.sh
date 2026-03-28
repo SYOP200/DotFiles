6889a835c216a3d0c563f7dbfbc8247f34e9e00e
SYOP200 <finnwhiskey@gmail.com>
2026-03-28 16:07:21 -0400

    Update wifi.sh to include subscription for wifi changes

--- sketchybar/items/wifi.sh
+++ sketchybar/items/wifi.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-
 sketchybar --add item wifi right                         \
            --set wifi    script="$PLUGIN_DIR/wifi.sh"    \
                          background.padding_right=12     \
-                         update_freq=5
+                         update_freq=5                   \
+sketchybar --subscribe wifi wifi_change                  \
