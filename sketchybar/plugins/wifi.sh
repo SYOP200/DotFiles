ca098996afce002422a5104225b6f729aaffdcf6
SYOP200 <finnwhiskey@gmail.com>
2026-03-28 16:10:20 -0400

    Refactor wifi.sh to use Apple Shortcut for SSID
    Now uses an Apple Shortcut to fetch SSID, must make one to work using the Shortcuts app on your Mac

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env sh
 
-CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
-SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
-CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
+#CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+#SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
+#CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
 
-if [ "$SSID" = "" ]; then
-  sketchybar --set $NAME label="Straylight" icon=􀙇 #icon=✈︎  
+#shortcuts run "Get Wi-Fi SSID"
+#output=$(shortcuts run "Get Wi-Fi SSID")
+#
+#sketchybar --set $NAME label=$output icon=􀙇
+#if $output = "-"
+#  sketchybar --set $NAME label="No Internet"
+
+#!/bin/bash
+
+# Run Apple Shortcut to get SSID
+SSID=$(shortcuts run "Get Wi-Fi SSID")
+
+# Trim whitespace
+SSID=$(echo "$SSID" | xargs)
+
+# Icons (you can swap these with Nerd Font icons if you prefer)
+ICON_CONNECTED="󰤨"
+ICON_DISCONNECTED="󰤭"
+
+if [ -z "$SSID" ] || [ "$SSID" = "No Wi-Fi" ]; then
+  sketchybar --set "$NAME" \
+    icon="$ICON_DISCONNECTED" \
+    label="No Internet"
 else
-  sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=􀙇
+  sketchybar --set "$NAME" \
+    icon="$ICON_CONNECTED" \
+    label="$SSID"
 fi
