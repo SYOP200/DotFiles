a552352e929c0c01d3ad18603b747c36ed627fbd
SYOP200 <finnwhiskey@gmail.com>
2026-03-28 16:22:13 -0400

    Update fish configuration and add startup script
    Removed greeting message and added startup script.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
-set fish_greeting "Hello, SYOP200"
+#!/bin/bash
+
+set fish_greeting ' '
 
 set -gx TERM xterm-256color
+fish_add_path -g ~/.local/bin
 
 # theme
 set -g theme_color_scheme terminal-dark
@@ -15,9 +18,12 @@ alias la "ls -A"
 alias ll "ls -l"
 alias lla "ll -A"
 alias g "git"
-alias c claude
+alias c "claude"
 alias claude-yolo "claude --dangerously-skip-permissions"
 alias update "brew upgrade"
 alias fishconfig "nvim ~/.config/fish/config.fish"
 command -qv nvim && alias vim nvim
+command -qv lvim && alias Lvim lvim
 
+# startup
+bash ~/startup/startup.sh
