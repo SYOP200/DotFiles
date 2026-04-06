28fec429d4ab0adb64a020fd7bf76b41cd57efe2
SYOP200 <finnwhiskey@gmail.com>
2026-04-06 18:16:10 -0400

    Delete sketchybar/.config/nvim directory

--- sketchybar/.config/nvim/init.lua
+++
@@ -1,2 +0,0 @@
-require("config.colorscheme").setup()
-require("config.lazy")
--- sketchybar/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
-local map = vim.keymap.set
-
-map({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>", { desc = "Write" })
-map("n", "<leader>fs", "<cmd>w<cr>", { desc = "Write" })
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
-map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
-map("n", "<Esc>", "<cmd>noh<cr><esc>", { desc = "Clear Search Highlight" })
